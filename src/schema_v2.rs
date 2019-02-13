5fd7a1d4e56502283f3f539ce351a49220476867
Tom Arrell <tom.arrell@gmail.com>
2019-02-13 17:48:50 +1300

    Added more fields

--- src/schema_v2.rs
+++ src/schema_v2.rs
@@ -1,16 +1,19 @@
 use serde::Deserialize;
 
 /// The root API struct.
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Debug, PartialEq, Default)]
 pub struct Spec {
+    pub swagger: String,
     pub info: Info,
+    pub host: Option<String>,
 }
 
 /// Basic information about the API.
-#[derive(Deserialize, Debug, PartialEq)]
+#[derive(Deserialize, Debug, PartialEq, Default)]
 pub struct Info {
     pub title: String,
     pub description: Option<String>,
+    #[serde(rename = "termsOfService")]
     pub terms_of_service: Option<String>,
     pub contact: Option<Contact>,
     pub license: Option<License>,
@@ -20,21 +23,21 @@ pub struct Info {
 /// Contact information regarding the API.
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct Contact {
-    pub name: String,
-    pub url: String,
-    pub email: String,
+    pub name: Option<String>,
+    pub url: Option<String>,
+    pub email: Option<String>,
 }
 
 /// The API's licensing information.
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct License {
     pub name: String,
-    pub url: String,
+    pub url: Option<String>,
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Info, Spec};
+    use super::{Info, Spec, Contact, License};
     use crate::deserialize;
 
     #[test]
@@ -43,6 +46,7 @@ mod tests {
         assert_eq!(
             deserialize(path),
             Ok(Spec {
+                swagger: "2.0".to_string(),
                 info: Info {
                     title: "Hello".to_string(),
                     description: None,
@@ -50,6 +54,34 @@ mod tests {
                     contact: None,
                     license: None,
                     version: "2.0".to_string(),
+                },
+                ..Default::default()
+            })
+        )
+    }
+
+    #[test]
+    fn info() {
+        let path = "examples/info.yaml";
+        assert_eq!(
+            deserialize(path),
+            Ok(Spec {
+                swagger: "2.0".to_string(),
+                host: Some("localhost:8080".to_string()),
+                info: Info {
+                    title: "Test Title".to_string(),
+                    description: Some("A test API".to_string()),
+                    terms_of_service: Some("Example terms".to_string()),
+                    contact: Some(Contact {
+                        name: Some("John Doe".to_string()),
+                        url: Some("https://johndoe.com".to_string()),
+                        email: Some("john@doe.com".to_string()),
+                    }),
+                    license: Some(License {
+                        name: "MIT".to_string(),
+                        url: Some("https://mit.com".to_string()),
+                    }),
+                    version: "1.0".to_string(),
                 }
             })
         )
